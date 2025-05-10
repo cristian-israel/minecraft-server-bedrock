c4b4e3ec0eafce00e91c4e544f2d32e3737cd5d8
cristian-israel <israel.dev@icloud.com>
2025-05-10 09:09:18 -0300

    fix: Corrigir nome do diretório no .gitignore e ajustar caminhos absolutos no helper paths.ts

--- src/helpers/paths.ts
+++ src/helpers/paths.ts
@@ -1,13 +1,9 @@
 import path from "path";
 
-// Caminhos
-export const CACHCE_DIR = path.join(__dirname, "..", "cache");
-export const BASE_DIR = path.resolve(path.dirname(__dirname));
-export const SERVER_DIR = path.join(BASE_DIR, "server");
-export const BACKUP_DIR = path.join(BASE_DIR, "backups");
+// Caminhos absolutos para os diretórios do projeto
+export const PREJECT_DIR = process.cwd();
+export const CACHCE_DIR = path.join(PREJECT_DIR, "cache");
 
-// URL de onde o servidor Bedrock será baixado
-export const MINECRAFT_URL =
-  "https://www.minecraft.net/pt-br/download/server/bedrock";
-export const MINECRAFT_DOWNLOAD_URL =
-  "https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-";
+// Caminhos absolutos para os diretórios do servidor
+export const SERVER_DIR = path.join(PREJECT_DIR, "minecraftServer");
+export const BACKUP_DIR = path.join(PREJECT_DIR, "backups");
---
+++ src/helpers/urls.ts
@@ -0,0 +1,5 @@
+// URL de onde o servidor Bedrock será baixado
+export const MINECRAFT_URL =
+  "https://www.minecraft.net/pt-br/download/server/bedrock";
+export const MINECRAFT_DOWNLOAD_URL =
+  "https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-";
